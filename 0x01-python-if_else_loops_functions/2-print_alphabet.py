d9ac4c365daddd27aa322793cdcb258fc6097520
Quinade00 <olaleyeadeola27@yahoo.com>
2023-12-01 03:51:43 -0800

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Print the alphabet in lowercase, not followed by a new line."""
-
-for letter in range(97, 123):
-    print("{}".format(chr(letter)), end="")
+"""A program that proints the ASCII alphabet,
+in lowercase, not followed by a new line."""
+for letters in range(97, 123):
+    print("{}".format(chr(letters)), end="")
