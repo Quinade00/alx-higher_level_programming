99f5a684d82900ce6ecc2717aa0c3e8f4bacb0f0
Quinade00 <olaleyeadeola27@yahoo.com>
2024-03-28 20:36:48 -0700

    I just added new files

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-"""
-Fetche https://alx-intranet.hbtn.io/status
-"""
-
-if __name__ == "__main__":
-        import urllib.request
-            req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-                with urllib.request.urlopen("req") as res:
-                            body = res.read()
-
-                                print("Body response:")
-                                    print("\t- Type: {}".format(body.__class__))
-                                        print("\t- Content: {}".format(body))
-                                            print("\t- Utf8 Content: {}".format(body.decode("ascii")))
+import urllib.request
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        a = type(html)
+        b = html.decode("utf-8")
+        print("Body response:")
+        print("\t- type: {}".format(a))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(b))
