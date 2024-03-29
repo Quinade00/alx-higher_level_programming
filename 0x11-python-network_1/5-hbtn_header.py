7bbbe800a2de1bcabdfbd972e580681bd1ed9595
Olaleye Adeola Blessing <139276186+Quinade00@users.noreply.github.com>
2024-03-28 20:54:58 -0700

    Delete 0x11-python-network_1 directory

--- 0x11-python-network_1/5-hbtn_header.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-'''Variable X-Request-Id In The Response Header'''
-
-if __name__ == "__main__":
-        import requests
-            import sys
-                url = sys.argv[1]
-                    response = requests.get(url)
-                        print(response.headers.get('X-Request-Id'))
