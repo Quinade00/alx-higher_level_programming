26b282c82d77f39230bf504a7339f93f78f94ef1
Quinade00 <olaleyeadeola27@yahoo.com>
2024-06-02 17:06:23 -0700

    I just added new files

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,14 @@
+-- Create user user_0d_1 if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+
+-- Grant all privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- List privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Create user user_0d_2 if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+
+-- List privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
